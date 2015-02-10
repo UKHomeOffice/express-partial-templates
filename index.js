370cef44a6079ea1ab33b9ad5aea6e2727261ab8
Leonard Martin <leonard.martin@digital.cabinet-office.gov.uk>
2015-02-10 11:53:19 +0000

    Use synchronous fs methods to simplify code
    Since it runs on startup there's no problem with blocking for a few milliseconds while we traverse the fs, and it makes the code orders of magnitude easier to understand.

--- index.js
+++ index.js
@@ -6,30 +6,13 @@ var _ = require('underscore'),
 var Parser = function (app) {
 
     var ext = '.' + app.get('view engine'),
-        partials;
+        partials = traverse(app.get('views'), ext);
 
     return function (req, res, next) {
-
-        function setPartials() {
-            res.locals.partials = res.locals.partials || {};
-            _.extend(res.locals.partials, partials);
-            next();
-        }
-
-        if (!partials) {
-            traverse(app.get('views'), ext, function (err, files) {
-                if (err) { return next(err); }
-
-                partials = files;
-                setPartials();
-            });
-        } else {
-            setPartials();
-        }
-
+        res.locals.partials = res.locals.partials || {};
+        _.extend(res.locals.partials, partials);
+        next();
     };
 };
-
-Parser.traverse = traverse;
 
 module.exports = Parser;
