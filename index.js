4fa69960834aef92c2f2e3791863fec41339902d
Leonard Martin <leonard.martin@digital.cabinet-office.gov.uk>
2015-03-12 13:08:16 +0000

    Support prefixes for partial template names
    If we want to use this more than once within an app - such as when a module wants to add its own partial templates to the stack - then to avoid conflicts with similarly named files we can add an optional prefix to namespace the partial names without needing to force the file structure.
    
    Also export partials with absolute rather than relative paths to facilitate use of multiple 'views' directories.

--- index.js
+++ index.js
@@ -3,10 +3,12 @@
 var _ = require('underscore'),
     traverse = require('./lib/traverse');
 
-var Parser = function (app) {
+var Parser = function (app, options) {
+
+    options = options || {};
 
     var ext = '.' + app.get('view engine'),
-        partials = traverse(app.get('views'), ext);
+        partials = traverse(app.get('views'), ext, options.prefix);
 
     return function (req, res, next) {
         res.locals.partials = res.locals.partials || {};
